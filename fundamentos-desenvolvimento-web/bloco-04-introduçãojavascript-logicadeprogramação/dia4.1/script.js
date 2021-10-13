a8a48c85d667c7a4bbcb25412281be39416c16be
Igor <igormelojunqueira@gmail.com>
2021-10-13 18:46:22 -0300

    Exercicio 5 do dia 4.1 e criaçao ddo dia 4.2

---
+++ fundamentos-desenvolvimento-web/bloco-04-introduçãojavascript-logicadeprogramação/dia4.1/script.js
@@ -0,0 +1,61 @@
+// Exercício 1
+
+const a = 10;
+const b = 3;
+
+console.log (a+b);
+console.log (a-b);
+console.log (a*b);
+console.log (a/b);
+console.log (a%b);
+
+// Exercício 2
+
+const c = 10;
+const d = 5;
+
+if (c>d) {
+  console.log (c);
+} else {
+console.log (d);
+}
+
+// Exercício 3 
+
+const e = 5;
+const f = 7;
+const g = 9;
+
+if (e > f && e > g) {
+  console.log(e);
+} else if (f > e && f > g) {
+  console.log(f);
+} else {
+  console.log(g);
+};
+
+// Exercício 4
+
+const h = 2;
+
+if (h > 0) {
+  console.log("Valor positivo");
+} else if (h < 0) {
+  console.log("Valor negativo")
+} else {
+  console.log("Zero");
+};
+
+
+// Exercício 5
+
+const ladoA = 80;
+const ladoB = 60;
+const ladoC = 40;
+
+if (ladoA + ladoB + ladoC === 180) {
+  console.log(true);
+} else {
+  console.log(false);
+};
+
